b0cf719814ef5acf777ecb61c03d3c17384ca5da
Aakash <aakashanand1995@gmail.com>
2024-06-22 18:48:48 +0530

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,14 @@
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Highlights from "./components/Highlight";
 const App = () => {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <main className="bg-black">
+      <Navbar />
+      <Hero />
+      <Highlights />
+    </main>
+  );
 };
 
 export default App;

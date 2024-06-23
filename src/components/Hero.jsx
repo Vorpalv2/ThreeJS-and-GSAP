e04afa1ba03868bc7e8f69afdd95de9677ecb3b4
Aakash <aakashanand1995@gmail.com>
2024-06-23 19:41:49 +0530

    fourth commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,51 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { heroVideo, smallHeroVideo } from "../utils";
+import { useState } from "react";
+import { useEffect } from "react";
+// console.log(heroVideo);
+
 const Hero = () => {
-  return <div>Hero</div>;
+  const [videoSrc, setVideoSrc] = useState(
+    window.innerWidth < 760 ? smallHeroVideo : heroVideo
+  );
+
+  const handleVideoSrcSet = () => {
+    if (window.innerWidth < 760) {
+      setVideoSrc(smallHeroVideo);
+    } else {
+      setVideoSrc(heroVideo);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", handleVideoSrcSet);
+
+    return () => {
+      window.removeEventListener("resize", handleVideoSrcSet);
+    };
+  }, []);
+
+  useGSAP(() => {
+    gsap.to(".hero-title", { opacity: 1, ease: "back.inOut", delay: 1.5 });
+  }, []);
+
+  return (
+    <div className="w-full nav-height bg-black relative">
+      <div className="h-5/6 w-full flex-center flex-col">
+        <p className="hero-title">IPhone 15 Pro</p>
+        <div className="md:w-10/12 w-9/12"></div>
+        <video
+          className="pointer-events-none"
+          src={videoSrc}
+          autoPlay
+          muted
+          playsInline={true}
+          key={videoSrc}
+        ></video>
+      </div>
+    </div>
+  );
 };
 
 export default Hero;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,28 @@
+import { appleImg, bagImg, searchImg } from "../utils";
+import { navLists } from "../constants/index";
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <header className="w-full py-5 sm:px-10 flex justify-between items-center">
+      <nav className="flex w-full screen-max-width">
+        <img src={appleImg} alt="logo" width={14} height={18} />
+        <div className="flex flex-1 justify-center max-sm:hidden">
+          {navLists.map((element) => (
+            <div
+              className="px-5 text-sm cursor-pointer text-gray hover:text-white transition-all"
+              key={element}
+            >
+              {element}
+            </div>
+          ))}
+        </div>
+        <div className="flex item-baseline gap-7 max-sm:justify-end max-sm:flex-1">
+          <img src={searchImg} alt="search" width={18} height={18} />
+          <img src={bagImg} alt="bag" width={18} height={18} />
+        </div>
+      </nav>
+    </header>
+  );
 };
 
 export default Navbar;

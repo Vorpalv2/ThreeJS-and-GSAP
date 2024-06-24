65dd30312a55a842137f8ea377dcf00f568d70a2
Aakash <aakashanand1995@gmail.com>
2024-06-24 13:29:56 +0530

    fifth commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -27,24 +27,35 @@ const Hero = () => {
   }, []);
 
   useGSAP(() => {
-    gsap.to(".hero-title", { opacity: 1, ease: "back.inOut", delay: 1.5 });
+    gsap.to(".hero-title", { opacity: 1, ease: "back.inOut", delay: 2 });
+    gsap.to("#cta", { opacity: 1, translateY: -40, delay: 2 });
   }, []);
 
   return (
-    <div className="w-full nav-height bg-black relative">
+    <section className="w-full nav-height bg-black relative">
       <div className="h-5/6 w-full flex-center flex-col">
         <p className="hero-title">IPhone 15 Pro</p>
-        <div className="md:w-10/12 w-9/12"></div>
-        <video
-          className="pointer-events-none"
-          src={videoSrc}
-          autoPlay
-          muted
-          playsInline={true}
-          key={videoSrc}
-        ></video>
+        <div className="md:w-10/12 w-9/12">
+          <video
+            className="pointer-events-none"
+            src={videoSrc}
+            autoPlay
+            muted
+            playsInline={true}
+            key={videoSrc}
+          ></video>
+        </div>
       </div>
-    </div>
+      <div
+        id="cta"
+        className="flex flex-col items-center opacity-0 translate-y-20"
+      >
+        <a href="#highlights" className="btn">
+          Buy
+        </a>
+        <p className="font-bold text-xl">From $199/months or $999</p>
+      </div>
+    </section>
   );
 };
 

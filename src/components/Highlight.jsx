4901b5e024a0ba9f5c6a09724b2ffe151409af13
Aakash <aakashanand1995@gmail.com>
2024-06-27 15:10:09 +0530

    thursday commit

--- src/components/Highlight.jsx
+++ src/components/Highlight.jsx
@@ -1,11 +1,12 @@
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 import { rightImg, watchImg } from "../utils";
+import VideoCarousel from "./VideoCarousel";
 
 const Highlight = () => {
   useGSAP(() => {
     gsap.to("#title", { opacity: 1, translateY: 0 });
-    gsap.to(".link", { opacity: 1, translateY: 0, duration: 1, stagger: 0.5 });
+    gsap.to(".link", { opacity: 1, translateY: 0, duration: 1, stagger: 0.25 });
   }, []);
   return (
     <section
@@ -29,6 +30,7 @@ const Highlight = () => {
             </p>
           </div>
         </div>
+        <VideoCarousel />
       </div>
     </section>
   );
